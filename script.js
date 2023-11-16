39921792242e7fa449bd5b67e4ef8a7f278e646d
Prafull Garg <gargprafull55@gmail.com>
2023-11-16 11:21:34 +0530

    Add Dynamic Functionality
    I have modified my rock-paper-scissors game to be more dynamic and provide real time user experience on UI through dom manipulation. previously all the information while playing game was shown on console, now everthing is shown on user interface.

--- script.js
+++ script.js
@@ -2,50 +2,106 @@
 let choices = ['rock', 'paper', 'scissors']
 
 
-// create a getComputerChoice fuction which will return any random choice from rock,paper,scissors
+// create a getComputerChoice function which will return any random choice from rock,paper,scissors
 function getComputerChoice(choices) {
 	let choice = Math.floor(Math.random()*choices.length)
 	return choices[choice]
 }
+const display = document.createElement('div')
+const displayChoices =document.createElement('div')
+const scores = document.createElement('div')
+const displayText =document.createElement('div')
+
+scores.textContent= 'Player : 0  | Computer : 0';
+displayChoices.textContent = 'Player Choice : Please select from below | Computer Choice : Computer will select ';
+
+display.appendChild(scores)
+display.appendChild(displayChoices)
+display.appendChild(displayText)
+
+const rock = document.createElement('button')
+const paper= document.createElement('button')
+const scissors = document.createElement('button')
+
+rock.innerHTML='';
+paper.innerHTML='';
+scissors.innerHTML='';
+
+rock.textContent ='ROCK';
+paper.textContent ='PAPER';
+scissors.textContent ='SCISSORS';
+
+
+const body = document.querySelector('body')
+
+rock.value = 'rock';
+paper.value ='paper'
+scissors.value ='scissors'
+
+body.appendChild(display)
+body.appendChild(rock)
+body.appendChild(paper)
+body.appendChild(scissors)
 
 // handle error if player inputs any value other than the given choices
+
+let playerScore=0;
+let computerScore=0;
+
 try{
-	// take player choice input from prompt and assign it to playerChoice variable
-	let playerChoice = prompt("Enter your choice").toLowerCase()
-	let computerChoice = getComputerChoice(choices)
+	//create function playround to play single round between computer and player and return the string to declare winner and specify who beats who
+	function playRound(){
+		// take player choice input from prompt and assign it to playerChoice variable
+		let playerChoice = this.value;
+		let computerChoice = getComputerChoice(choices)
        		if(!choices.includes(playerChoice)){
        			throw new Error("Invalid Choice : Enter an valid choice") // throw customized error to handle logical error
 		 }
-console.log(playerChoice +" "+ computerChoice)
-//create fuction playround to play single round between computer and player and return the string to declare winner and specify who beats who
-function playRound(playerChoice,computerChoice){
+	displayChoices.textContent =`Player Choice ${playerChoice} | Computer Choice ${computerChoice}`		 
+
 	if(playerChoice === computerChoice){
-		return `Draw : ${playerChoice} equals ${computerChoice}`
+		displayText.textContent=`Round Draw : ${playerChoice} equals ${computerChoice}`
+		computerScore+=1
+		playerScore+=1
 	}else if((playerChoice === 'rock') && (computerChoice === 'paper')){
-		return `You Lose : ${computerChoice} beats ${playerChoice}`
-	}else if((playerChoice === 'rock') && (computerChoie === 'scissors')){
-		return `You Won : ${playerChoice} beats ${computerChoice}`
+		computerScore+=1
+		displayText.textContent=`You Lose Round : ${computerChoice} beats ${playerChoice}`
+	}else if((playerChoice === 'rock') && (computerChoice === 'scissors')){
+		playerScore+=1
+		displayText.textContent=`You Won Round : ${playerChoice} beats ${computerChoice}`
 	}else if((playerChoice === 'paper') && (computerChoice === 'scissors')){
-                return `You Lose : ${computerChoice} beats ${playerChoice}`
-	}else if((playerChoice === 'paper') && (coputerChoice === 'rock')){
-		return `You Won : ${playerChoice} beats ${computerChoice}`
+	    computerScore+=1
+		displayText.textContent= `You Lose Round: ${computerChoice} beats ${playerChoice}`
+	}else if((playerChoice === 'paper') && (computerChoice === 'rock')){
+		playerScore+=1
+		displayText.textContent=`You Won Round : ${playerChoice} beats ${computerChoice}`
 	}else if ((playerChoice === 'scissors') && (computerChoice === 'paper')){
-		return `You Won : ${playerChoice} beats ${computerChoice}`
+		playerScore+=1
+		displayText.textContent=`You Won Round : ${playerChoice} beats ${computerChoice}`
 	}else {
-		return `You Lose : ${computerChoice} beats ${playerChoice}`
+		computerScore+=1
+		displayText.textContent=`You Lose Round : ${computerChoice} beats ${playerChoice}`
+	}
+
+	scores.textContent= ` Player : ${playerScore} | Computer : ${computerScore} `
+	if((playerScore === 5) || (computerScore === 5)){
+		if(playerScore === computerScore){
+			displayText.textContent= 'Game Draw'
+		}else if(playerScore === 5){
+			displayText.textContent= 'You Won The Game'
+		}else {
+			displayText.textContent= 'You Lost The Game'
+		}
+		playerScore=0
+		computerScore=0
 	}
 
 }
 
-function game(){
-	console.log(playRound(playerChoice, computerChoice))
-	console.log(playRound(playerChoice, computerChoice))
-	console.log(playRound(playerChoice, computerChoice))
-console.log(playRound(playerChoice, computerChoice))
-console.log(playRound(playerChoice, computerChoice))
+rock.addEventListener('click', playRound)
+paper.addEventListener('click', playRound)
+scissors.addEventListener('click', playRound)
 
-
-}
 
 }catch(error){
         console.log(error.name)
@@ -53,6 +109,3 @@ console.log(playRound(playerChoice, computerChoice))
         console.log(error.stack)
 }
 
-
-
-game();
